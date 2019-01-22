7d5b94ded8bcbe86a9815162578849c4c89a7d09
primoashbee <ashbee.morgado@icloud.com>
2019-01-22 21:12:59 +0800

    2nd Commit
    Middleware for admin
    Database Update (+brgy)

---
+++ brgy.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.19 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `baranggays` (
+	`id` int (15),
+	`name` varchar (2295),
+	`created_at` timestamp ,
+	`updated_at` timestamp ,
+	`isDeleted` tinyint (1)
+); 
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('1','Poblacion','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('2','Cataning','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('3','Bagumbayan','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('4','Talisay','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('5','Malabia','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('6','San Jose\r\n','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('7','Ibayo','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('8','Dona Francisca','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('9','Cupang Proper','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('10','Cupang North','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('11','Cupang West','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('12','Sibacan','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('13','Tuyo','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('14','Puerto Rivas Ibaba','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('15','Puerto Rivas Itaas','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('16','Tortugas','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('17','Central','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('18','Tenejero','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('19','Camacho','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('20','Bagong Silang','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('21','Puerto Rivas Lote','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('22','Dangcol','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('23','Cabog-Cabog','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('24','Tanato','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
+insert into `baranggays` (`id`, `name`, `created_at`, `updated_at`, `isDeleted`) values('25','Munting Batangas','2019-01-22 21:11:43','2019-01-22 21:11:43','0');
